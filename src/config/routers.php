1cfdae6783e1ac6da6e7aecb3f3aefee452cde02
Nurlan Tulemisov <n.tulemisov@corp.mail.ru>
2022-03-13 12:39:09 +0300

    f

--- src/config/routers.php
+++ src/config/routers.php
@@ -1,17 +1,9 @@
 <?php
 
 use Nurlantulemisov\ServiceCounter\Controller\CounterController;
-use Nurlantulemisov\ServiceCounter\Controller\TestController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes) {
-    $routes->add('blog_list', '/blog')
-        ->controller([TestController::class, 'list']);
-
-    $routes->add('blog_get', '/blog/{id}')
-        ->controller([TestController::class, 'get'])
-        ->methods(['GET', 'HEAD']);
-
     $routes->add('count', '/count')
         ->controller([CounterController::class, 'count'])
         ->methods(['GET']);
